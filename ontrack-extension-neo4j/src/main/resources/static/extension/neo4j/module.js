0b438eedaa0c89ef23f8b9636d1fe07c9bc0ef0a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-02-05 13:58:10 +0100

    #282 Neo4J export - dialog

--- ontrack-extension-neo4j/src/main/resources/static/extension/neo4j/module.js
+++ ontrack-extension-neo4j/src/main/resources/static/extension/neo4j/module.js
@@ -1,16 +1,17 @@
 angular.module('ontrack.extension.neo4j', [
-    'ot.service.core'
+    'ot.service.core',
+    'ot.service.form'
 ])
 // Routing
     .config(function ($stateProvider) {
         $stateProvider.state('neo4j-export', {
             url: '/extension/neo4j/export',
-            templateUrl: 'extension/neo4j/export.tpl.html',
+            templateUrl: 'extension/neo4j/neo4j-export.tpl.html',
             controller: 'Neo4JExportActionCtrl'
         });
     })
     // Controller
-    .controller('Neo4JExportActionCtrl', function ($scope, ot) {
+    .controller('Neo4JExportActionCtrl', function ($http, $scope, ot, otFormService) {
         // View definition
         var view = ot.view();
         view.title = "Export to Neo4J";
@@ -18,6 +19,11 @@ angular.module('ontrack.extension.neo4j', [
             // Closing to home page
             ot.viewCloseCommand('/home')
         ];
-        // TODO Loads and displays the form
+        // Loads and displays the form
+        ot.pageCall($http.get('extension/neo4j/export')).then(function (form) {
+            $scope.exportForm = form;
+            // Prepares the form for display
+            $scope.exportData = otFormService.prepareForDisplay(form);
+        });
     })
 ;
